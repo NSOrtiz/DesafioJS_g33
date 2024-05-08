6c0744ec315ab0ab3993a1902876122bbc040112
Luis Casas <luisscasas45@gmail.com>
2024-05-08 17:27:09 -0600

    FEAT/ search created

--- js/tags.js
+++
@@ -1,8 +0,0 @@
-let randomTag =(lista)=>{
-    item = Math.floor(Math.random()*(lista.length))
-    return lista[item]
-}
-
-let tags = ['csharp','dotnet','programming','tools','beginers','react','java','tutorial','python','php','javascript','webdev','ruby','rails','design']
-
-console.log(randomTag(tags))
