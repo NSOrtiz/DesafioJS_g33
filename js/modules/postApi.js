f431e2646da83b37b6aa860ba445bca706796e14
Natalia Ortiz <nsol1094@gmail.com>
2024-05-08 17:44:17 -0600

    changes on postApi.js

--- js/modules/postApi.js
+++ js/modules/postApi.js
@@ -1,1 +1,14 @@
+const sendPost = async (postObject) =>{
+    let sendData = await fetch(
+        "https://postform-4f43d-default-rtdb.firebaseio.com/Post/.json",
+    {
+        method: "POST",
+        body: JSON.stringify(postObject),
+    })
+    let data =await sendData.json()
+    console.log(data)
 
+}
+
+
+export { sendPost}
--- js/postForm.js
+++ js/postForm.js
@@ -1,18 +1,5 @@
-const getPost = async () =>{
-    let response = await fetch("https://reto-js-bd894-default-rtdb.firebaseio.com/.json")
-    let Post = await response.json()
-    console.log(Post)
-    
-    let keys = Object.keys(Post) //manda a llamar metodo object 
-    //console.log(keys) 
 
-    let postsArray = keys.map((key) => {
-        return {...Post[key], key}
-    })
-    console.log(postsArray)
-    return postsArray 
-}
-
+import {sendPost} from "./modules/postApi.js"
 
 const updateLabels = () =>{
     let inputText = document.getElementById("textInput");
@@ -67,29 +54,9 @@ saveBtn.addEventListener("click", (event)=>{
   window.open('../index.html','_self')
 })
 
-const sendPost = async (postObject) =>{
-    let sendData = await fetch(
-        "https://reto-js-bd894-default-rtdb.firebaseio.com/.json",
-    {
-        method: "POST",
-        body: JSON.stringify(postObject),
-    })
-    let data =await sendData.json() //desempaquetar info enviada 
-    console.log(data)
 
-}
 
-const getProductData = async (postKey) => {
 
-    //promesa --> await --> promesa cumplida --> muestra la info de la promesa
-    let response = await fetch(`https://reto-js-bd894-default-rtdb.firebaseio.com/${postKey}.json`)
-
-    //También es una promesa --> await --> promesa cumplida y brinda los datos por el método JSON 
-    let data = await response.json()
-
-    
-    return data
-}
 
 let closeBtn = document.getElementById("btn-close") 
 
@@ -98,4 +65,3 @@ closeBtn.addEventListener("click", async(event)=>{
   event.preventDefault() 
   window.open('../index.html','_self')
 })
-
