ee96ce06e356ef70c6cd05c1d3f41c706efe5bea
Luis Casas <luisscasas45@gmail.com>
2024-05-07 20:53:59 -0600

    FEAT/ latest and top, created

---
+++ js/tags.js
@@ -0,0 +1,8 @@
+let randomTag =(lista)=>{
+    item = Math.floor(Math.random()*(lista.length))
+    return lista[item]
+}
+
+let tags = ['csharp','dotnet','programming','tools','beginers','react','java','tutorial','python','php','javascript','webdev','ruby','rails','design']
+
+console.log(randomTag(tags))
